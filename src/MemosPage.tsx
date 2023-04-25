da3e7ba69054b56794d73f084d9ef37ea1388551
Teoman Nejat Wiedenbeck <t.wiedenbeck.1@campus.tu-berlin.de>
2023-04-25 21:07:27 +0200

    add validation and design

--- src/MemosPage.tsx
+++ src/MemosPage.tsx
@@ -5,6 +5,7 @@ import { Memo } from './scss/types.ts'
 import memoDB from './scss/db.ts'
 
 
+
 function MemoPage() {
   const navigate = useNavigate()
   const memo = useLoaderData() as Memo
@@ -19,11 +20,27 @@ function MemoPage() {
       progress: { value: string };
     };
     const text = target.text.value;
-    console.log(text)
+    //console.log(text)
     const date = target.deadline.value;
-    console.log(date)
+    //console.log(date)
     const progress = target.progress.value;
-    console.log(progress)
+
+    if (!isValidText(text)) {
+      alert('Bitte gib einen Text von höchsten 160 Zeichen an')
+      return
+    }
+    
+
+    if (!isValidProgress(Number(progress))) {
+      alert('Bitte gib dein Progress als Zahl zwischen 0 und 100 an')
+      return
+    }
+
+    if (!isValidDate(new Date(date))) {
+      alert('Bitte gib ein gültiges Datum ein')
+      return
+    }
+    //console.log(progress)
     const i = memoDB.indexof(memo)
     memoDB.memos[i].text = text
     memoDB.memos[i].deadline = new Date(date)
@@ -31,34 +48,47 @@ function MemoPage() {
     memoDB.toCookies()
     navigate(-1)
   }
+  const isValidText = (t: string) => {
+    return 161 > t.length && t.length > 0
+  }
+
+  const isValidProgress = (p: number) => {
+    return p >= 0 && p <= 100
+  }
+
+  const isValidDate = (d: Date) => {
+    return d instanceof Date && !isNaN(d.getTime());
+  }
+
   return (
     <>
       <h1>MemoPage</h1>
+     
       {/* // todo list */}
       <p>{memo.deadline.toDateString()}</p>
       {/* creates a form to edit the memo and shows already existing Information */}
       <form id='editForm' onSubmit={handle}>
         <div className="mb-3">
           <label htmlFor="text" className="form-label">Deine Memo</label>
-          <input defaultValue={memo.text} type="text" className="form-control" id="text" aria-describedby="text" name="text"/>
+          <input defaultValue={memo.text} type="text" className="form-control" id="text" aria-describedby="text" name="text" />
         </div>
         <div className="mb-3">
           <label htmlFor="deadline" className="form-label">Deadline Format: {memo.deadline.toLocaleDateString()}</label>
-          <input defaultValue={memo.deadline.toLocaleDateString()} type="text" className="form-control" id="deadline" name="deadline"/>
+          <input defaultValue={memo.deadline.toLocaleDateString()} type="text" className="form-control" id="deadline" name="deadline" />
         </div>
-        
         <div className="mb-3">
-          <label htmlFor="progress" className="form-label">Dein Prodress</label>
+          <label htmlFor="progress" className="form-label">Dein Progress zwischen 1 und 100 %</label>
           <input defaultValue={memo.progress} type="number" className="form-control" id="progress" name="progress" />
         </div>
-        <button type="submit" className="btn btn-primary">save</button>
-        <button type='button' onClick={(event : React.SyntheticEvent) =>{
+        <button type="submit"  className="btn btn-primary m-2">save</button>
+        
+        <button type='button' onClick={(event: React.SyntheticEvent) => {
           event.preventDefault()
-          console.log('remove')
+          //console.log('remove')
           memoDB.removeMemo(memo)
           memoDB.toCookies()
           navigate(-1)
-        }} className="btn btn-primary">remove</button>
+        }} className="btn btn-danger">remove</button>
       </form>
     </>
   )
